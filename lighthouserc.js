1baea237953ef7db8de34e451fca939e950dd373
Barbara Cristina Conceição <bconceicao@fanatics.com>
2024-03-16 15:49:26 -0300

    Setup score to 80

--- lighthouserc.js
+++ lighthouserc.js
@@ -26,7 +26,7 @@ module.exports = {
                 'categories:pwa': 'off',
                 'categories:seo': 'off',
                 'categories.best-practices': 'off',
-                'categories.accessibility': ['error', { minScore: 0.9 }]
+                'categories.accessibility': ['error', { minScore: 0.8 }]
             }
         }
     },
