8ef32cb7c035131f7ab294d3e38f9206081317f1
Barbara Cristina Conceição <bconceicao@fanatics.com>
2024-03-16 13:32:01 -0300

    Make the tests pass on workflow

--- cypress/e2e/sauceLogin.cy.js
+++ cypress/e2e/sauceLogin.cy.js
@@ -2,7 +2,7 @@ describe('Login no Sauce Labs', () => {
 
   it('realizar login', () => {
     cy.visit('/');
-    cy.get('[data-test="username"]').type('barbara_cabral');
+    cy.get('[data-test="username"]').type('standard_user');
     cy.get('[data-test="password"]').type('secret_sauce');
     cy.get('[data-test="login-button"]').click();
   })
